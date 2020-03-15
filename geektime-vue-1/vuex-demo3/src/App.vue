c5934b4e72cf6a74f33d649d2b109b0308008a92
dengdairiluo <1020447548@qq.com>
2020-03-16 00:37:08 +0800

    进度 + 1

--- geektime-vue-1/vuex-demo3/src/App.vue
+++ geektime-vue-1/vuex-demo3/src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>购物车示例</h1>
+    <p>账号: {{email}}</p>
+    <hr>
+    <h2>产品</h2>
+    <ProductList/>
+    <hr>
+    <ShoppingCart/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from 'vuex'
+import ProductList from './components/ProductList.vue'
+import ShoppingCart from './components/ShoppingCart.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  computed: mapState({
+    email: state => state.userInfo.email
+  }),
+  components: { ProductList, ShoppingCart },
 }
 </script>
 
